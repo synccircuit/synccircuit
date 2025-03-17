1d58885e5ca627bec24766ac747582f4e542859e
woozystudio <woozysstudio23@gmail.com>
2025-03-16 22:12:45 -0600

    feat: create messageConfig.ts

---
+++ apps/synccircuit/src/messageConfig.ts
@@ -0,0 +1,9 @@
+export const messageConfig = {
+  embedErrorMessage: "An error occurred!",
+
+  devOnly: "This command can only be executed by developers.",
+  testMode: "This is in development and cannot be executed in this server.",
+
+  userNoPerms: "You do not have enough permissions to execute this.",
+  botNoPerms: "I do nnot have enough permissions to execute this.",
+};
