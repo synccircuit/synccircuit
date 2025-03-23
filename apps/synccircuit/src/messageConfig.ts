6919d0845252efd8240ac72c7ad62446df855704
woozystudio <woozysstudio23@gmail.com>
2025-03-23 13:26:19 -0600

    fix: spaces and erros in messageConfig

--- apps/synccircuit/src/messageConfig.ts
+++ apps/synccircuit/src/messageConfig.ts
@@ -2,9 +2,10 @@ export const messageConfig = {
   embedErrorMessage: "`❌` An error occurred!",
 
   devOnly: "`❌` This command can only be executed by developers.",
-  testMode: "`❌`This is in development and cannot be executed in this server.",
-  userNoPerms: "`❌`You do not have enough permissions to execute this.",
-  botNoPerms: "`❌`I do not have enough permissions to execute this.",
+  testMode:
+    "`❌` This is in development and cannot be executed in this server.",
+  userNoPerms: "`❌` You do not have enough permissions to execute this.",
+  botNoPerms: "`❌` I do not have enough permissions to execute this.",
   hasHigherRolePos:
     "`❌` This user has a higher role than you have or has the same role as you have.",
   unableToInteractWithYourself: "`❌` You cannot do that to yourself.",
