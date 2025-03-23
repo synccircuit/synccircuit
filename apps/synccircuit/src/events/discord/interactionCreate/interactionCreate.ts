c69ba094c6de00347778e3f5565821678f034487
woozystudio <woozysstudio23@gmail.com>
2025-03-23 13:25:53 -0600

    fix: remove validations because doesn't work

--- apps/synccircuit/src/events/discord/interactionCreate/interactionCreate.ts
+++ apps/synccircuit/src/events/discord/interactionCreate/interactionCreate.ts
@@ -1,14 +1,21 @@
 import {
   ButtonInteraction,
   ChatInputCommandInteraction,
+  ColorResolvable,
   ContextMenuCommandInteraction,
+  EmbedBuilder,
   Events,
+  GuildMember,
+  MessageFlags,
+  PermissionsBitField,
 } from "discord.js";
 import Event from "@/classes/Event";
 import Command from "@/classes/Command";
 import IntegratedClient from "@/classes/IntegratedClient";
 import Button from "@/classes/Button";
 import ContextMenu from "@/classes/ContextMenu";
+import { LogColors } from "@/consts/loggingSymbols";
+import { messageConfig } from "@/messageConfig";
 
 export default class InteractionCreate extends Event {
   constructor(client: IntegratedClient) {
@@ -24,6 +31,9 @@ export default class InteractionCreate extends Event {
       | ButtonInteraction
       | ContextMenuCommandInteraction
   ) {
+    const createEmbed = (color: ColorResolvable, description: string) =>
+      new EmbedBuilder().setColor(color).setDescription(description);
+
     if (interaction.isChatInputCommand()) {
       const command: Command = this.client.commands.get(
         interaction.commandName
@@ -35,6 +45,41 @@ export default class InteractionCreate extends Event {
           interaction.reply({ content: "outdated command", ephemeral: true }),
           this.client.commands.delete(interaction.commandName)
         );
+
+      const userPermissions = command.default_member_permissions
+        ? new PermissionsBitField(command.default_member_permissions).toArray()
+        : [];
+
+      const botPermissions = command.default_bot_permissions
+        ? new PermissionsBitField(command.default_bot_permissions).toArray()
+        : [];
+
+      const member = interaction.member as GuildMember;
+      for (const permission of userPermissions) {
+        if (!member.permissions.has(permission)) {
+          const rEmbed = createEmbed(
+            LogColors.Error,
+            messageConfig.userNoPerms
+          );
+          await interaction.reply({
+            embeds: [rEmbed],
+            flags: MessageFlags.Ephemeral,
+          });
+          return;
+        }
+      }
+
+      const bot = interaction.guild?.members.me;
+      for (const permission of botPermissions) {
+        if (!bot?.permissions.has(permission)) {
+          const rEmbed = createEmbed(LogColors.Error, messageConfig.botNoPerms);
+          await interaction.reply({
+            embeds: [rEmbed],
+            flags: MessageFlags.Ephemeral,
+          });
+          return;
+        }
+      }
 
       try {
         const subCommandGroup = interaction.options.getSubcommandGroup(false);
@@ -59,6 +104,20 @@ export default class InteractionCreate extends Event {
           this.client.buttons.delete(interaction.customId)
         );
 
+      if (
+        interaction.message.interactionMetadata &&
+        interaction.message.interactionMetadata.user.id !== interaction.user.id
+      ) {
+        const rEmbed = createEmbed(
+          LogColors.Error,
+          messageConfig.cannotUseButton
+        );
+        await interaction.reply({
+          embeds: [rEmbed],
+          flags: MessageFlags.Ephemeral,
+        });
+      }
+
       try {
         return button.execute(interaction);
       } catch (error) {
@@ -80,6 +139,43 @@ export default class InteractionCreate extends Event {
           }),
           this.client.contextMenus.delete(interaction.commandName)
         );
+
+      const userPermissions = contextMenu.default_member_permissions
+        ? new PermissionsBitField(
+            contextMenu.default_member_permissions
+          ).toArray()
+        : [];
+
+      const botPermissions = contextMenu.default_bot_permissions
+        ? new PermissionsBitField(contextMenu.default_bot_permissions).toArray()
+        : [];
+
+      const member = interaction.member as GuildMember;
+      for (const permission of userPermissions) {
+        if (!member.permissions.has(permission)) {
+          const rEmbed = createEmbed(
+            LogColors.Error,
+            messageConfig.userNoPerms
+          );
+          await interaction.reply({
+            embeds: [rEmbed],
+            flags: MessageFlags.Ephemeral,
+          });
+          return;
+        }
+      }
+
+      const bot = interaction.guild?.members.me;
+      for (const permission of botPermissions) {
+        if (!bot?.permissions.has(permission)) {
+          const rEmbed = createEmbed(LogColors.Error, messageConfig.botNoPerms);
+          await interaction.reply({
+            embeds: [rEmbed],
+            flags: MessageFlags.Ephemeral,
+          });
+          return;
+        }
+      }
 
       try {
         const context = `${interaction.commandName}${interaction.commandType}`;
